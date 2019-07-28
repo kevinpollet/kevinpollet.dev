17079f74ad71ac4017bcdc28763e1cd39b7d5a02
kevinpollet <pollet.kevin@gmail.com>
2019-07-28 15:55:48 +0400

    Add posts path prefix

---
+++ content/initial-post/index.md
@@ -0,0 +1,5 @@
+---
+title: Initial post
+description: Description
+date: 2019-07-28
+---
--- gatsby-node.js
+++ gatsby-node.js
@@ -60,12 +60,12 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode });
+    const postPath = createFilePath({ node, getNode });
 
     createNodeField({
       name: `slug`,
       node,
-      value,
+      value: `posts${postPath}`,
     });
   }
 };
