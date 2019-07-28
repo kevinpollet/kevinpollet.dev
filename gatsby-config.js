460f04a97c92e8629d402dda543e660beb047998
kevinpollet <pollet.kevin@gmail.com>
2019-07-28 17:11:25 +0400

    Use netlify site URL in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     title: "Kevin Pollet",
     author: "Kevin Pollet",
     description: "Blog posts by Kevin Pollet",
-    siteUrl: "https://kevinpollet.dev",
+    siteUrl: "https://inspiring-hermann-b9dd46.netlify.com/",
     social: {
       twitter: "@kevinpollet",
       gitHub: "kevinpollet",
