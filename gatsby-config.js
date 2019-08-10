6d19d8bc5dfdb76d3f85ca7845a86782b52ee5a3
kevinpollet <pollet.kevin@gmail.com>
2019-08-10 22:34:18 +0400

    Generate anchors for markdown headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,7 @@ module.exports = {
             },
           },
           "gatsby-remark-copy-linked-files",
+          "gatsby-remark-autolink-headers",
           "gatsby-remark-prismjs",
         ],
       },
